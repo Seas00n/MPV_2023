1c267b59dcb255de408e463a533d7423738d9be4
Seas00n <60185289+Seas00n@users.noreply.github.com>
2023-03-17 01:09:49 +0800

    Initial commit

---
+++ Pros_Ctrl_RTOS/App/Inc/debug_uart.h
@@ -0,0 +1,14 @@
+/*
+ * debug_uart.h
+ *
+ *  Created on: Mar 14, 2023
+ *      Author: root
+ */
+
+#ifndef INC_DEBUG_UART_H_
+#define INC_DEBUG_UART_H_
+
+void debugPrint(char *fmt,...);
+
+
+#endif /* INC_DEBUG_UART_H_ */
